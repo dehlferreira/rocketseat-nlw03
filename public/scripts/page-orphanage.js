76aaac6472bbb84920c8d6ab6f88432d67cba228
André Luiz Ferreira <andre.ferreira@labsit.io>
2020-10-17 11:54:29 -0300

    feat(Creating Database): Creating database and refactoring code to receive data from the database

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -6,8 +6,12 @@ const options = {
   zoomControl: false,
 }
 
+// get values from html
+const lat = document.querySelector('span[data-lat]').dataset.lat;
+const lng = document.querySelector('span[data-lng]').dataset.lng;
+
 //create map
-const map = L.map('mapid', options).setView([-23.6388255,-46.5149171], 15);
+const map = L.map('mapid', options).setView([lat, lng], 15);
 
 //create and add tileLayer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
@@ -20,8 +24,8 @@ const icon = L.icon({
   popupAnchor: [170, 2]
 });
 
-
-L.marker([-23.6388255,-46.5149171], { icon })
+// create and add marker
+L.marker([lat,lng], { icon })
   .addTo(map)
 
   /* image gallery */
